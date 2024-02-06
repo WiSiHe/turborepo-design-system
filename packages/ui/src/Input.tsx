898225b286df26e131606286e01697ff7f174c01
Henrik WIlhelm Sissener <hws902@gmail.com>
2024-02-06 16:11:15 +0100

    feat(ui): Add Input component
    - Added a new file `Input.tsx` to the `ui` package
    - Implemented an `Input` component with a label prop
    - The `Input` component renders an input element

---
+++ packages/ui/src/Input.tsx
@@ -0,0 +1,9 @@
+export interface InputProps {
+  label: string;
+}
+
+export const Input = ({ label }: InputProps) => {
+  return <input />;
+};
+
+Input.displayName = 'Input';
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,3 @@
 export { Button, type ButtonProps } from './button';
-
 export { Checkbox, type CheckboxProps } from './Checkbox';
+export { Input, type InputProps } from './Input';
