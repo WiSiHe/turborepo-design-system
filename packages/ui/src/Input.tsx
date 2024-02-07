e01fb6e53f6b497f09e41db6d6184dc5594ddb68
Henrik WIlhelm Sissener <hws902@gmail.com>
2024-02-07 11:33:34 +0100

    dwa

--- packages/ui/src/Input.tsx
+++ packages/ui/src/Input.tsx
@@ -7,7 +7,7 @@ export const Input = ({ label }: InputProps) => {
     <div>
       <label>{label}</label>
       <input />
-      <p>error: test message</p>
+      <p>error: test message dwa</p>
     </div>
   );
 };
